e6d337c2b0fa7da85e898638f199e3cb36fac266
dmeskis <dmeskis@gmail.com>
2018-08-25 18:34:53 -0600

    Completed job comment model test

--- spec/models/job_comment_spec.rb
+++ spec/models/job_comment_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe JobComment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe JobComment, type: :model do
+  describe "validations" do
+    context 'valid attributes' do
+      it "is valid with content" do
+        company = Company.create!(name: "Turing")
+        job = Job.create!(title: "Title", company: company, level_of_interest: 80, description: "Wahoo", city: "Denver")
+        comment = job.job_comments.create!(content: "This is an example")
+        expect(comment).to be_valid
+      end
+    end
+    context "invalid attributes" do
+      it "is invalid without content" do
+        comment = JobComment.create()
+        expect(comment).to be_invalid
+      end
+    end
+  end
 end
