ced908d39d68fb96669206c258af66a034b330c0
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-26 12:50:19 -0600

    Adds test for sorting jobs by location

---
+++ spec/features/jobs/user_sees_jobs_sorted_by_location_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+describe "User visits /jobs?sort=location" do
+  it "should show all jobs sorted by location" do
+    category_1 = Category.create!(title: "Testing")
+    category_2 = Category.create!(title: "Dev")
+    company_1 = Company.create!(name: "Google")
+    company_2 = Company.create!(name: "Turing")
+    job_1 = company_1.jobs.create!(title: "Software Developer",
+                                  description: "Do Stuff",
+                                  level_of_interest: 3,
+                                  city: "Denver",
+                                  category_id: category_1.id)
+    job_2 = company_2.jobs.create!(title: "Testing Developer",
+                                  description: "Test Stuff",
+                                  level_of_interest: 4,
+                                  city: "San Fran",
+                                  category_id: category_1.id)
+    job_2 = company_2.jobs.create!(title: "Testing Developer",
+                                  description: "Test Stuff",
+                                  level_of_interest: 4,
+                                  city: "Denver",
+                                  category_id: category_1.id)
+
+    visit '/jobs?sort=location'
+
+    within("#1") do
+      expect(page).to have_content("Denver")
+    end
+
+    within("#2") do
+      expect(page).to have_content("Denver")
+    end
+    within("#3") do
+      expect(page).to have_content("San Fran")
+    end
+  end
+end
