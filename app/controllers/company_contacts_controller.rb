5ab3658efd6bfe3d7d0e7dbbfe0601bb4aa4d412
dmeskis <dmeskis@gmail.com>
2018-08-27 16:46:59 -0600

    Fixed forms to not have hidden fields and modified controllers to reflect this change

--- app/controllers/company_contacts_controller.rb
+++ app/controllers/company_contacts_controller.rb
@@ -1,20 +1,21 @@
 class CompanyContactsController < ApplicationController
 
   def create
-    @company_contact = CompanyContact.new(company_contact_params)
+    @company = Company.find(params[:company_id])
+    @company_contact = @company.company_contacts.create!(company_contact_params)
     if @company_contact.save
       flash[:success] = "#{@company_contact.name} added to contacts!"
-      redirect_to company_path(@company_contact.company_id)
+      redirect_to company_path(@company_contact.company)
     else
       flash[:failure] = "Failed to add company contact."
-      redirect_to company_path(@company_contact.company_id)
+      redirect_to company_path(@company_contact.company)
     end
   end
 
   private
 
   def company_contact_params
-    params.require(:company_contact).permit(:name, :position, :email, :company_id)
+    params.require(:company_contact).permit(:name, :position, :email)
   end
 
 end
--- app/controllers/job_comments_controller.rb
+++ app/controllers/job_comments_controller.rb
@@ -1,18 +1,19 @@
 class JobCommentsController < ApplicationController
   def create
-    @job_comment = JobComment.new(job_comment_params)
+    @job = Job.find(params[:job_id])
+    @job_comment = @job.job_comments.create!(job_comment_params)
     if @job_comment.save
       flash[:success] = "Comment posted!"
-      redirect_to job_path(params[:job_comment][:job_id])
+      redirect_to job_path(params[:job_id])
     else
       flash[:failure] = "Comment failed to post."
-      redirect_to job_path(params[:job_comment][:job_id])
+      redirect_to job_path(params[:job_id])
     end
   end
 
   private
 
   def job_comment_params
-    params.require(:job_comment).permit(:content, :job_id)
+    params.require(:job_comment).permit(:content)
   end
 end
