5e9eebe7e5c608d7f1e7a7511ef5d9ccf078d7fb
dmeskis <dmeskis@gmail.com>
2018-08-27 19:19:41 -0600

    Changed test to be less hard corded

--- spec/features/dashboard/user_can_see_top_three_jobs_by_average_interest_spec.rb
+++ spec/features/dashboard/user_can_see_top_three_jobs_by_average_interest_spec.rb
@@ -8,17 +8,18 @@ describe 'User can see top 3 companies by interest' do
       company_2 = Company.create(name: "Microsoft")
       company_3 = Company.create(name: "Twitter")
       company.jobs.create(title: "Title 1", level_of_interest: 3, city: "Denver", category: category)
-      company.jobs.create(title: "Title 2", level_of_interest: 5, city: "Denver", category: category)
+      company.jobs.create(title: "Title 2", level_of_interest: 2, city: "Denver", category: category)
       company_2.jobs.create(title: "Title 1", level_of_interest: 2, city: "Denver", category: category)
       company_2.jobs.create(title: "Title 2", level_of_interest: 4, city: "Denver", category: category)
       company_3.jobs.create(title: "Title 1", level_of_interest: 1, city: "Denver", category: category)
       company_3.jobs.create(title: "Title 2", level_of_interest: 1, city: "Denver", category: category)
+      values = Company.top_companies_by_interest
 
       visit dashboard_path
-
-      expect(page).to have_content("4.0 Stars")
-      expect(page).to have_content("3.0 Stars")
-      expect(page).to have_content("1.0 Star")
+      save_and_open_page
+      expect(page).to have_content("Google #{values["Google"]} Stars")
+      expect(page).to have_content("Microsoft #{values["Microsoft"]} Stars")
+      expect(page).to have_content("Twitter #{values["Twitter"]} Star")
     end
   end
 end
