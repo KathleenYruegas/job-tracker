5b6fb9636bc8198f0845af1b1da755d858216d22
Kathleen Yruegas <kathleenecn@gmail.com>
2018-08-27 21:20:39 -0600

    Removes comments, changes formats

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,8 +1,8 @@
 class Job < ApplicationRecord
   validates :title, :level_of_interest, :city, presence: true
   belongs_to :company
-  has_many :job_comments
   belongs_to :category
+  has_many :job_comments
 
   def self.city_counter
     cities = get_cities
