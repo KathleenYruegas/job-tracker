74143f03f3c3bf6356768fdb8641d3d7478971ed
dmeskis <dmeskis@gmail.com>
2018-08-27 18:42:04 -0600

    Added method for creating company average interest hash

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,4 +2,14 @@ class Company < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   has_many :jobs, dependent: :destroy
   has_many :company_contacts
+
+
+  def self.top_companies_by_interest
+    top_3_companies = select("companies.*, avg(level_of_interest) AS average_interest").joins(:jobs).group(:company_id, :id).order("average_interest DESC").limit(3)
+    top_3_companies.inject({}) do |company_average, company|
+      company_average[company.name] = company.jobs.average(:level_of_interest)
+      company_average
+    end
+  end
+
 end
